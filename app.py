c4f587050a6386c3745d88efef56efce7796e7f7
ngonsoto <nico-fgs@hotmail.com>
2024-11-26 23:07:27 -0300

    fix: linted exisiting files

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-appVersion = "v0.0.1"
-senseBoxes = "615a7b227ea3fe001c7daa7e", "6132aaf6d5bb40001c370189", "6184b16751eeff001ba45c98"
+"""
+A basic flask application which uses opensensemap.org data
+"""
 
 from flask import Flask
-from temperature import *
+from temperature import avg_temperature
+
+# pylint: disable=C0103
+appVersion = "v0.0.1"
+senseBoxes = "615a7b227ea3fe001c7daa7e", "6132aaf6d5bb40001c370189", "6184b16751eeff001ba45c98"
 
 app = Flask(__name__)
 
 @app.route("/")
 def version():
+    """shows app version"""
     return appVersion
 
 @app.route('/temperature')
 def temperature():
-    return "{:.2f}".format(avgTemperature(senseBoxes))
+    """shows avg temperature of all locations passed as arguments"""
+    return f"{avg_temperature(senseBoxes):.2f}"
--- temperature.py
+++ temperature.py
@@ -1,22 +1,37 @@
-def avgTemperature(boxes):
-    # importing the requests library
-    import requests
-    
-    totalTemperatur = 0
+"""
+Returns average temperature of all senseboxes
 
-    for id in boxes:
+Parameters:
+    boxes (list): List of senseboxes to be queried
+
+Returns:
+    avgTemperatur (float): Average temperature of queried sensors
+"""
+
+# importing the requests library
+import logging
+import requests
+
+def avg_temperature(boxes):
+    """returns average temperature of {boxes}"""
+    total_temperatur = 0
+
+    for boxid in boxes:
         # api-endpoint
-        URL = "https://api.opensensemap.org/boxes/" + id + "?format=json"
+        url = "https://api.opensensemap.org/boxes/" + boxid + "?format=json"
 
         # sending get request and saving the response as response object
-        r = requests.get(url = URL)
+        try:
+            r = requests.get(url, timeout=10)
+        except requests.exceptions.Timeout:
+            logging.error("timeout raised")
 
         # extracting data in json format
         data = r.json()
-        temperaturStr = data['sensors'][2]['lastMeasurement']['value']
+        temperatur_str = data['sensors'][2]['lastMeasurement']['value']
 
         # adding current box temperature to total
-        totalTemperatur+=float(temperaturStr)
+        total_temperatur+=float(temperatur_str)
 
     # return average temperature
-    return(totalTemperatur/len(boxes))
+    return total_temperatur/len(boxes)
