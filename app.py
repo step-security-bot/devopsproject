a37f6f72acbb129be3e019282c8ce0696b8aa5d3
ngonsoto <nico-fgs@hotmail.com>
2024-11-27 02:28:51 -0300

    feat: added unit test for endpoints

--- app.py
+++ app.py
@@ -2,21 +2,20 @@
 A basic flask application which uses opensensemap.org data
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from temperature import avg_temperature
 
-# pylint: disable=C0103
-appVersion = "v0.0.1"
-senseBoxes = "615a7b227ea3fe001c7daa7e", "6132aaf6d5bb40001c370189", "6184b16751eeff001ba45c98"
+APP_VERSION = "v0.0.1"
+senseboxes = "615a7b227ea3fe001c7daa7e", "6132aaf6d5bb40001c370189", "6184b16751eeff001ba45c98"
 
 app = Flask(__name__)
 
 @app.route("/")
 def version():
     """shows app version"""
-    return appVersion
+    return jsonify(version = APP_VERSION)
 
 @app.route('/temperature')
 def temperature():
     """shows avg temperature of all locations passed as arguments"""
-    return f"{avg_temperature(senseBoxes):.2f}"
+    return jsonify(avg_temperature = f"{avg_temperature(senseboxes):.2f}")
---
+++ tests/test_app.py
@@ -0,0 +1,40 @@
+"""
+Unit tests for Flask application
+"""
+
+# Import sys module for modifying Python's runtime environment
+import sys
+# Import os module for interacting with the operating system
+import os
+# Import pytest for writing and running tests
+import pytest
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Import the Flask app instance from the main app file
+from app import app
+
+@pytest.fixture
+def client():
+    """Set up a test client for the app with setup and teardown logic."""
+    print("\nSetting up the test client")
+    with app.test_client() as client:
+        yield client  # This is where the testing happens
+    print("Tearing down the test client")
+
+def test_home(client):
+    """Test the home route."""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json == {"version": "v0.0.1"}
+
+def test_about(client):
+    """Test the temperature route."""
+    response = client.get('/temperature')
+    assert response.status_code == 200
+
+def test_non_existent_route(client):
+    """Test for a non-existent route."""
+    response = client.get('/non-existent')
+    assert response.status_code == 404
