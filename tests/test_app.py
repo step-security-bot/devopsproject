59ed954cb4ef44c1f979f057d3350c82ff131913
ngonsoto <nico-fgs@hotmail.com>
2024-11-27 03:05:17 -0300

    fix: disabled warning on flask test

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,6 +16,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Import the Flask app instance from the main app file
 from app import app
 
+# pylint: disable=W0621
 @pytest.fixture
 def client():
     """Set up a test client for the app with setup and teardown logic."""
